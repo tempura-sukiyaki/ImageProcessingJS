d410c8d9828ad287f8c7bd21f3d0188b9233af59
tempura-sukiyaki <10625603+tempura-sukiyaki@users.noreply.github.com>
2018-01-29 12:07:39 +0900

    refactoring

--- threshold/threshold.js
+++ threshold/threshold.js
@@ -1,48 +1,34 @@
-//var canvas = document.getElementById("canvas")
-//document.getElementById("backyard")
+function loadImage( imageBitmap )
+  {
+    const canvas = document.createElement( 'canvas' );
+    canvas.width = imageBitmap.width;
+    canvas.height = imageBitmap.height;
 
-//var ctx = canvas.getContext('2d')
+    const context = canvas.getContext( '2d' );
+    context.drawImage( imageBitmap, 0, 0 );
 
-var rawimg = new Image();
-var url = null;
+    const imageData = context.getImageData( 0, 0, canvas.width, canvas.height );
+    imageData.data.forEach( ( value, index, array ) =>
+      { array[ index ] = value * 2 < 0xff ? 0x00 : 0xff; }
+    );
+    context.putImageData( imageData, 0, 0 );
 
-var width = 10;
-var height = 10;
+    document.getElementById( 'result' ).src = canvas.toDataURL( 'image/png' );
+  }
 
-document.getElementById("raw_image").onchange = function(e) {
-    console.log("0");
-    var backyard = document.createElement("canvas");
-    var bctx = backyard.getContext('2d')
+function changeFile( event )
+  {
+    if ( event.target.files.length )
+      { createImageBitmap( event.target.files[ 0 ] ).then( loadImage ); }
+    else
+      { document.getElementById( 'result' ).src = ''; }
+  }
 
-    var reader = new FileReader();
-    reader.onload = function(ev) {
-        rawimg.onload = function () {
-            width = rawimg.width;
-            height = rawimg.height;
-            backyard.width = width;
-            backyard.height = height;
-            bctx.drawImage(rawimg, 0,0);
-
-            console.log("1");
-            var imgd = bctx.getImageData(0, 0, width, height);
-            
-            imgd.data.forEach( function (value, index, array) {
-                if(array[index]>127) {
-                    array[index] = 255;
-                }
-                else {
-                    array[index] = 0;
-                }
-            });
-            
-            bctx.putImageData(imgd, 0, 0);
-            url = backyard.toDataURL('image/png');
-
-            document.getElementById("result_img").src = url;
-
-        }
-        rawimg.src = ev.target.result;
-    }
-    reader.readAsDataURL(e.target.files[0])
-};
-
+/*
+// <input onchange> を使用しない場合
+document.addEventListener( 'DOMContentLoaded', event =>
+  {
+    document.getElementById( 'file' ).addEventListener( 'change', changeFile );
+  }
+);
+*/
